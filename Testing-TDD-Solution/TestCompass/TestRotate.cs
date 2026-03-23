e61930f30ec20f4adaabe7e7f3d8ef407fc813d5
Zitian Harper <Zitian.harper@icloud.com>
2026-03-23 12:36:26 +0000

    lunch

---
+++ Testing-TDD-Solution/TestCompass/TestRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+namespace TestCompass
+{
+    [TestClass]
+    public sealed class TestRotate
+    {
+        [Test]
+        public void Rotate_FromNorth_TurnRight_ReturnsEast()
+        {
+        //Arrange - declare point and direction and rotate method
+        var compass = new Compass();
+        
+        //Act - point and position after Rotate 
+        var result = Compass.Rotate(Point.North, Direction.Right);
+
+
+        //Assert - what should be returned after rotate
+        Assert.AreEqual(Point.East, result);
+
+    }
+}
+}
