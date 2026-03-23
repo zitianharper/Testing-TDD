996bbfd1bd5300e74dd6ef6bc572e971818ed081
Zitian Harper <Zitian.harper@icloud.com>
2026-03-23 11:44:03 +0000

    task 2 complete

---
+++ Testing-TDD-Solution/Testing-TDD/Compass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_TDD
+{
+
+
+    internal class Compass
+    {
+        public Point CurrentPoint { get; set; }
+
+     
+
+        public static Point Rotate(Point point, Direction direction)
+            {
+            return Point.South;
+            }
+    }
+}
---
+++ Testing-TDD-Solution/Testing-TDD/Point.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_TDD
+{
+    public enum Point
+    {
+
+    }
+}
